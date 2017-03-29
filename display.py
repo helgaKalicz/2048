80b096f25b5a6f7b0b4b9e53c8a666724ecf6c64
helgaKalicz <kalicz.helga@gmail.com>
2017-03-29 15:12:50 +0200

    display new version

--- display.py
+++ display.py
@@ -1,52 +1,27 @@
-def display_table():	
-    
-    # Handling several digit numbers
-	one_digit = '   │  '
-	two_digits = '  │  '
-	three_digits = ' │  '
-	four_digits = '│  '
-	partBetween = []
+a = ['0', '0', '0', '0']
+b = ['0', '0', '0', '0']
+c = ['0', '0', '0', '0']
+d = ['0', '0', '0', '0']
 
-	for i in range(len(a)):
-		if len(a[i]) == 1:
-			partBetween.append(one_digit)
-		elif len(a[i]) == 2:
-			partBetween.append(two_digits)
-		elif len(a[i]) == 3:
-			partBetween.append(three_digits)
-		elif len(a[i]) == 4:
-			partBetween.append(four_digits)
-	
-	for i in range(len(b)):
-		if len(b[i]) == 1:
-			partBetween.append(one_digit)
-		elif len(b[i]) == 2:
-			partBetween.append(two_digits)
-		elif len(b[i]) == 3:
-			partBetween.append(three_digits)
-		elif len(b[i]) == 4:
-			partBetween.append(four_digits)
 
-	for i in range(len(c)):
-		if len(c[i]) == 1:
-			partBetween.append(one_digit)
-		elif len(c[i]) == 2:
-			partBetween.append(two_digits)
-		elif len(c[i]) == 3:
-			partBetween.append(three_digits)
-		elif len(c[i]) == 4:
-			partBetween.append(four_digits)
-	
-	for i in range(len(d)):
-		if len(d[i]) == 1:
-			partBetween.append(one_digit)
-		elif len(d[i]) == 2:
-			partBetween.append(two_digits)
-		elif len(d[i]) == 3:
-			partBetween.append(three_digits)
-		elif len(d[i]) == 4:
-			partBetween.append(four_digits)
-
+def display_table(list_a):
+    one_digit = '   │  '
+    two_digits = '  │  '
+    three_digits = ' │  '
+    four_digits = '│  '
+    partBetween = []
+    for i in range(len(list_a)):
+        if len(list_a[i]) == 1:
+            partBetween.append(one_digit)
+        elif len(list_a[i]) == 2:
+            partBetween.append(two_digits)
+        elif len(list_a[i]) == 3:
+            partBetween.append(three_digits)
+        elif len(list_a[i]) == 4:
+            partBetween.append(four_digits)
     return partBetween
 
-print(display_table())
+print(display_table(a))
+print(display_table(b))
+print(display_table(c))
+print(display_table(d))
